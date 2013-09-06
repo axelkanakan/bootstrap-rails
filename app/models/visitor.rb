4e78bfb89f5ccf636b36fcaff6a8604c019dedc9
krillen <okulby@yahoo.com>
2013-09-06 01:08:16 -0700

    added form

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,2 +1,10 @@
 class Visitor < ActiveRecord::Base
+	
+	has_no_table
+	column :favorite, :string
+	column :comment, :string
+	validates_presence_of :favorite
+
+	IMAGE_LABELS = ['San Francisco', 'Sidney', 'Paris']
+
 end
